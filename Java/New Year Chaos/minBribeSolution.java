601284d2f45cc71a5d59193c9e80a8afe88bfcdb
crobinson89 <coryscottrobinson@gmail.com>
2018-09-06 19:46:01 -0500

    Update minBribeSolution.java

--- Java/New Year Chaos/minBribeSolution.java
+++ Java/New Year Chaos/minBribeSolution.java
@@ -3,41 +3,35 @@ Project: New Year Chaos
 This: minBribeSolution.java
 Author: C Robinson
 Date: 06 Sep 2018
-Purpose: Solve New Year Chaos problem. What this problem uses:
-                - Bubble Sort
-                - Counter for moves per number
-                - Counter for minimum moves
-                - For/if to print out results
-                
- This solution does not pass all 12 tests.
- */
+Purpose: Solve New Year Chaos problem. What this problem does:
+                - Iterates from end
+                - Checks if any element is more than 2 moves away
+                - Increases counter for each element that should move
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 public class minBribeSolution {
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
-        int n = q.length;
-        int c[] = new int[n];
-        int b = 0, d = 0;
-        for (int i = 0; i < n-1; i++){
-            for (int j = 0; j < n-i-1; j++){
-                if (q[j] > q[j+1]){
-                    c[q[j]-1]++;
-                    int temp = q[j];
-                    q[j] = q[j+1];
-                    q[j+1] = temp;
-                    b++;
-                }
-            }
-        }
-        for(int i = 0; i < c.length; i++){
-            if(c[i] > 2){
+        int n = 0;
+        for(int i = q.length - 1; i >= 0; i--){
+            if(q[i] - (i+1) > 2){
                 System.out.println("Too chaotic");
-                d++;
-                break;
+                return;
+            }
+            for(int j = Math.max(0,q[i]-2); j < i; j++){
+                if(q[j] > q[i]){
+                    n++;
+                }                    
             }
         }
-        if(d < 1){
-            System.out.println(b);
-        }       
+        System.out.println(n);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
